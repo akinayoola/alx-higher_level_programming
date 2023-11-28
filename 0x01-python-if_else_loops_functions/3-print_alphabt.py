3397521ef40722ac79448f25856caa6a88e4c0ff
akinayoola <akingunloye006@gmail.com>
2023-11-28 14:11:20 -0800

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) not in '113 101':
+    if chr(letter) != 113 and chr(letter) != 103:
         print("{}".format(chr(letter)), end="")
