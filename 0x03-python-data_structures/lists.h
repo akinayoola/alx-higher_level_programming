f58f50cc85c4df170ff57f51f0e660c895f3129f
akinayoola <akingunloye006@gmail.com>
2023-12-03 10:41:43 -0800

    python

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+#endif
