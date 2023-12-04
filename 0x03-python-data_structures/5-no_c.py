ef69e561095bb3718815e005dcdde9139e962694
akinayoola <akingunloye006@gmail.com>
2023-12-03 16:43:08 -0800

    python

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    get_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C'
+        get_string += char
+        return get_string
