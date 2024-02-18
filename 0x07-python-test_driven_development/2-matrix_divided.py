d06aa9445454dc63acd2dc358829fdcebcd61b69
akinayoola <akingunloye006@gmail.com>
2024-02-18 07:07:41 -0800

    testenv

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    
+    """Divides element of a given matrix.
+
+    Args:
+        matrix (list): a matrix of rows and column
+        div (a float or integer)
+
+    Raises:
+        TypeError: matrix must be a matrix (list of lists) of integers/floats
+        TypeError: Each row of the matrix must have the same size
+        TypeError: div must be a number
+        ZeroDivisionError: division by zero
+
+    Returns a new matrix
+    """
+
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or 
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+
+    return (new_matrix)
+
+
+
+
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This are tasks on Python - Test-driven development
