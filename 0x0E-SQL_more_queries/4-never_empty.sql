9a608fad8a1721c3af725a616e2fac237eeee6a7
akinayoola <akingunloye006@gmail.com>
2024-03-13 09:20:54 -0700

    sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,2 @@
 -- a script that creates the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_nul(
-	id INT DEFAULT 1,
-	name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS id_not_nul (id INT DEFAULT 1, name VARCHAR(256));
