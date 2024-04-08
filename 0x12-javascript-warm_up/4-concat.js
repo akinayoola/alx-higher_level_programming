15b43e16e651684805339abc210faa8905b514bd
akinayoola <akingunloye006@gmail.com>
2024-04-08 14:35:53 -0700

    java

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-	console.log('Nothing to Print')
-} else {
-	console.log('${process.argv[2]} + 'is' + ${process.argv[3]})
-i}
+console.log(process.argv[2] + 'is' + process.argv[3])
