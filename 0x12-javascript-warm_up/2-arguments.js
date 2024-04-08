c093990a4ebc03e521c296c251e0892da173320d
akinayoola <akingunloye006@gmail.com>
2024-04-08 13:20:25 -0700

    java

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function message () {
+	if (argument.length === 0) {
+		console.log('No argument')
+	}
+	else if (argument.length === 1) {
+		console.log('Argument found')
+	}
+	else {
+		console.log('Arguments found')
+	}
+}
