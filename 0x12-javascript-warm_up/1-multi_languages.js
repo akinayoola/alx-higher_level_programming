a7bd7c94f4c291244c6952294bcf357eec5b2de9
akinayoola <akingunloye006@gmail.com>
2024-04-08 12:46:16 -0700

    java

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let output1 = “C is fun”;
+let output2 = “Python is cool”;
+let output3 = “JavaScript is amazing”;
+console.log(output1);
+console.log(output2);
+console.log(output3);
